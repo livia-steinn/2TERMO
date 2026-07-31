e2f88585e024ff1cce99bf0937b99bb84339a774
livia-steinn <liviasteinpereira03@gmail.com>
2026-07-31 16:58:17 -0300

    Atualização 1

---
+++ BACKEND/Aula_01/app2.js
@@ -0,0 +1,4 @@
+const nome = "Lívia";
+let idade = 16;
+console.log (nome);
+console.log(idade);
---
+++ BACKEND/Aula_01/app3.js
@@ -0,0 +1,9 @@
+const  escola = "SENAI";
+const  nota = 9.5;
+const estaAprovado = true;
+
+console.log("Conteúdo:", escola, "| Tipo", typeof escola);
+console.log("Conteúdo:", nota, "| Tipo", typeof nota);
+console.log("Conteúdo:", estaAprovado, "| Tipo", typeof estaAprovado);
+
+
